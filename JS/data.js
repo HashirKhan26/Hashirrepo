f8184cd9ca8cdec59b0c1ec9d010b14827d3d1c8
Student <lab-209@aptech-msg.edu.pk>
2022-10-31 21:51:01 +0500

    This is my first commit

---
+++ JS/data.js
@@ -0,0 +1,16 @@
+var Products = [
+    {"Id":0 , "Name":"" ,"Category":"" ,"Description":"","img":""},
+
+{"ID":1 ,"Name":"Bata" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"1.jfif"},
+{"ID":2 ,"Name":"Service" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"2.jfif"},
+{"ID":3 ,"Name":"Indure" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"3.jfif"},
+{"ID":4 ,"Name":"Jordan" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"logo.png"},
+{"ID":5 ,"Name":"Bata" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"1.jfif"},
+{"ID":6 ,"Name":"Service" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"2.jfif"},
+{"ID":7 ,"Name":"Indure" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"3.jfif"},
+{"ID":8 ,"Name":"Jordan" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"logo.png"},
+{"ID":9 ,"Name":"Bata" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"1.jfif"},
+{"ID":10 ,"Name":"Service" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"2.jfif"},
+{"ID":11 ,"Name":"Indure" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"3.jfif"},
+{"ID":12 ,"Name":"Jordan" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"logo.png"},
+]
